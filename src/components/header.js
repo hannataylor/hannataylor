960f4e87b9edee01f0781894c9d0c64e2269058a
Hanna Taylor <hanna.taylor91@gmail.com>
2019-10-20 20:50:28 -0400

    Fix mobile

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,20 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <nav className="navbar container" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <Link to="/" className="link navbar-item navbar-logo">{ siteTitle }</Link>
+    <nav className="navbar container" id="top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand is-hidden-mobile">
+        <Link to="/#top" className="link navbar-item navbar-logo">{ siteTitle }</Link>
       </div>
-      <div className="navbar-start">
+
+      <div className="navbar-end">
+        <div className="navbar-brand">
+        <Link to="/#top" className="link navbar-item is-hidden-tablet">HT</Link>
+        </div>
+        <div className="navbar-end">
         <Link to="/#about" className="link navbar-item">About</Link>
         <Link to="/#resume" className="link navbar-item">Resume</Link>
         <Link to="/#contact" className="link navbar-item">Contact</Link>
+        </div>
       </div>
     </nav>
   </header>
