b3ae9575c0f2d23c3fe9ef81103f5c23c8192283
Hanna Taylor <hanna.taylor91@gmail.com>
2020-01-07 12:14:20 -0500

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `hanna-taylor-personal-website`,
+        short_name: `hanna-taylor`,
         start_url: `/`,
         background_color: `black`,
         theme_color: `black`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ht-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
