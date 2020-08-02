d5871532a84d1fa531b28afaa665125e39aef2e2
Hanna Taylor <hanna.taylor91@gmail.com>
2020-08-02 19:58:43 -0400

    Remove instagram

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,6 @@ const Footer = () => (
         <div className="social-icons" aria-label="Social Media Links">
             <a className="icon" href="https://github.com/hannataylor" target="_blank" rel="noopener noreferrer" aria-label="View my Github profile"><i className="fab fa-github"></i></a>
             <a className="icon" href="https://www.linkedin.com/in/hannataylor" target="_blank" rel="noopener noreferrer" aria-label="Connect on LinkedIn"><i className="fab fa-linkedin"></i></a>
-            <a className="icon" href="https://www.instagram.com/hanna__taylor/" target="_blank" rel="noopener noreferrer" aria-label="Follow on Instagram"><i className="fab fa-instagram"></i></a>
         </div>
         <div className="copyright">
             <p>Copyright © {new Date().getFullYear()}</p>
